409b717b9c0ed87b63e3783a5956f83f541ce479
MartinVladkov <martin_vladkov@abv.bg>
2022-07-28 10:44:51 +0300

    UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Settings from './Settings';
+import Timer from "./Timer";
+import {useState} from "react";
+import SettingsContext from './SettingsContext';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    const [showSettngs, setShowSettings] = useState(false);
+    const [workMinutes, setWorkMinutes] = useState(25);
+    const [breakMinutes, setBreakMinutes] = useState(5);
+
+    return (
+    <main>
+        <SettingsContext.Provider value={{
+            workMinutes,
+            breakMinutes,
+            setWorkMinutes,
+            setBreakMinutes,
+        }}>
+        {showSettngs ? <Settings/> : <Timer />}
+        </SettingsContext.Provider>
+    </main>
   );
 }
 
---
+++ src/Settings.js
@@ -0,0 +1,36 @@
+import ReactSlider from "react-slider";
+import SettingsContext from "./SettingsContext";
+import './slider.css';
+import {useContext} from 'react';
+import BackButton from "./BackButton";
+
+function Settings() {
+    const settingsInfo = useContext(SettingsContext);
+
+    return(
+        <div style={{textAlign:'left'}}>
+            <label>work: {settingsInfo.workMinutes}:00 min.</label>
+            <ReactSlider className={'slider'} 
+            thumbClassName={'thumb'}
+            trackClassName={'track'}
+            value={settingsInfo.workMinutes}
+            onChange={newValue => settingsInfo.setWorkMinutes(newValue)}
+            min={1}
+            max={90}/>
+            <label>break: {settingsInfo.breakMinutes}:00 min.</label>
+            <ReactSlider className={'slider'} 
+            thumbClassName={'thumb'}
+            trackClassName={'track'}
+            value={settingsInfo.breakMinutes}
+            onChange={newValue => settingsInfo.setBreakMinutes(newValue)}
+            min={1}
+            max={90}/>
+
+            <div style={{textAlign:'center', marginTop:'20px'}}>
+                <BackButton/>
+            </div>
+        </div>
+    );
+}
+
+export default Settings;
